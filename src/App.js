27d49c6dee595a1730c60f1a30328607a82d8e02
Nicolay Moreno <nicolay.mherrera@gmail.com>
2021-12-02 22:08:53 -0500

    Route and Switch

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import './App.css';
 import { ConceptosBasicos } from './components/ConceptosBasicos';
 
 function App() {
--- src/components/ConceptosBasicos.js
+++ src/components/ConceptosBasicos.js
@@ -1,9 +1,24 @@
-import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 export const ConceptosBasicos = () => {
     return (
         <div>
             <h2>Conceptos Basicos</h2>
+
+            <Router>
+                <Switch>
+                    <Route exact path='/'>
+                        <h3>Home</h3>
+                        <p>Bienvenid@s al tema de las Rutas en React</p>
+                    </Route>
+                    <Route exact path='/acerca'>
+                        <h3>Acerca</h3>
+                    </Route>
+                    <Route exact path='/contacto'>
+                        <h3>Contacto</h3>
+                    </Route>
+                </Switch>
+            </Router>
         </div>
     );
 };
